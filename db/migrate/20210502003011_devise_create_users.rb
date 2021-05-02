74b40a19638f1e29fb7a478b6a46fe88ade9f5a7
sanohitomi800 <h.tommy.0830s@gmail.com>
2021-05-02 10:28:15 +0900

    新規登録完了

--- db/migrate/20210502003011_devise_create_users.rb
+++ db/migrate/20210502003011_devise_create_users.rb
@@ -7,7 +7,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
       t.string :name,               null: false
-      t.integer :genre_id,          null: false         
+      t.integer :genre_id                
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 2021_05_02_003011) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "name", null: false
-    t.integer "genre_id", null: false
+    t.integer "genre_id"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
